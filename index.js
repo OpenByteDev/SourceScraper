8f8cf21bbf7bb0dd46c954568df867715fd07c34
Openbyte <development.openbyte@gmail.com>
2018-02-11 20:09:50 +0100

    added classes.js

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
-const Error = require('./lib/Error.js');
-const Source = require('./lib/Source.js');
-const Info = require('./lib/Info.js');
-const Scrap = require('./lib/Scrap.js');
-const Scrapper = require('./lib/Scrapper.js');
-const LinkScrapper = require('./lib/LinkScrapper.js');
-const StreamScrapper = require('./lib/StreamScrapper.js');
-const ScrapperList = require('./lib/ScrapperList.js');
-
-const scrappers = require('./lib/scrappers.js');
-const runners = require('./lib/runners.js');
-const config = require('./lib/config.js');
-
-
 module.exports = {
-    scrappers: scrappers,
-    runners: runners,
-    classes: {
-        Error: Error,
-        Source: Source,
-        Info: Info,
-        Scrap: Scrap,
-        Scrapper: Scrapper,
-        LinkScrapper: LinkScrapper,
-        StreamScrapper: StreamScrapper,
-        ScrapperList: ScrapperList
-    },
-    config: config
+    scrappers: require('./lib/scrappers.js'),
+    runners: require('./lib/runners.js'),
+    classes: require('./lib/classes.js'),
+    config: require('./lib/config.js')
 };
