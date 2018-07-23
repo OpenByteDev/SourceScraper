d23447e611a60e57dc9c9d10ea7b49a12f103f4e
OpenByte <development.openbyte@gmail.com>
2018-07-23 20:47:39 +0200

    added masteranime package; regenerated docs; updated .ignores:

---
+++ packages/masteranime/lib/MasterAnimeScrapper.ts
@@ -0,0 +1,27 @@
+import { Hoster, HosterData, HosterScrapper } from 'sourcescrapper-core';
+
+import { MasterAnimeAPI } from 'masteranime-api';
+
+export interface MasterAnimeHosterData extends HosterData {
+    data: MasterAnimeAPI;
+}
+export class MasterAnimeScrapper extends HosterScrapper {
+    public name: string = 'masteranime';
+    public domains: string[] = ['masterani.me'];
+    public urlPattern: RegExp = /https?:\/\/www\.masterani\.me\/anime\/watch\/(\d+-(?:\w+-)+\w+)\/(\d+)/;
+    public async run(url: string): Promise<MasterAnimeHosterData> {
+        const data = await MasterAnimeAPI.getEpisodeDetailedFromUrl(url);
+        return {
+            data,
+            title: data.anime.info.title,
+            hosters: data.mirrors.map(e => new Hoster({
+                name: e.host.name,
+                quality: e.quality,
+                url:
+                (e.host.embed_prefix || '').replace(/\\\//g, '/') +
+                e.embed_id +
+                (e.host.embed_suffix || ''),
+            }))
+        };
+    }
+}
