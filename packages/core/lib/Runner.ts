214cb60ebc5c63179afe15d5857f04875df6c8cf
OpenByte <development.openbyte@gmail.com>
2018-08-01 23:17:41 +0200

    updated Runner related classes

--- packages/core/lib/Runner.ts
+++ packages/core/lib/Runner.ts
@@ -1,18 +1,40 @@
+import { IStaticThis } from './types';
+
 import objectMerge = require('object-merge');
 
-export type RunnerOptions = object;
-export interface RunnerArgs<T> {
+export type IRunnerOptions = object;
+
+export interface IRunnerArgs<O extends IRunnerOptions = IRunnerOptions> {
     url: string;
-    scrapper: RunnerExec<T>;
-    options?: any;
+    options?: O;
 }
-export type RunnerExec<T> = (RunnerArgs) => Promise<T>;
+
+export type IRunnerExec<T, O extends IRunnerOptions, A extends IRunnerArgs<O>> = (args: A) => Promise<T>;
+
+export interface IRunner<
+        T,
+        O extends IRunnerOptions = IRunnerOptions,
+        A extends IRunnerArgs<O> = IRunnerArgs<O>,
+        E extends IRunnerExec<T, O, A> = IRunnerExec<T, O, A>> {
+    run: (url: string, scrapper: E , options?: O) => Promise<T>;
+}
+
 export abstract class Runner<
-        T = any,
-        E extends RunnerExec<T> = RunnerExec<T>,
-        O extends RunnerOptions = RunnerOptions> {
+        T,
+        O extends IRunnerOptions = IRunnerOptions,
+        A extends IRunnerArgs<O> = IRunnerArgs<O>,
+        E extends IRunnerExec<T, O, A> = IRunnerExec<T, O, A>> implements IRunner<T, O, A, E> {
+    public static async run<
+        T,
+        O extends IRunnerOptions = IRunnerOptions,
+        A extends IRunnerArgs<O> = IRunnerArgs<O>,
+        E extends IRunnerExec<T, O, A> = IRunnerExec<T, O, A>,
+        R extends IRunner<T, O, A, E> = IRunner<T, O, A, E>>(
+            this: IStaticThis<R>, url: string, scrapper: E, options?: O): Promise<T> {
+        return new this().run(url, scrapper, options);
+    }
     public abstract defaultOptions: O;
-    public async abstract run(url: string, scrapper: E, options?: O): Promise<T>;
+    public abstract async run(url: string, scrapper: (args: A) => Promise<T>, options?: O): Promise<T>;
     protected getOptions(options?: O): O {
         return typeof options === 'undefined' ?
             this.defaultOptions :
