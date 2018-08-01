4c7314cb8aac6a0dd738f24a6531771c1c8f4eac
OpenByte <development.openbyte@gmail.com>
2018-08-01 23:20:55 +0200

    Changed SimpleDomScrapper to extend from RunnerScrapper

--- packages/dom-runner/lib/SimpleDomScrapper.ts
+++ packages/dom-runner/lib/SimpleDomScrapper.ts
@@ -1,47 +1,43 @@
-import { DomRunner } from './DomRunner';
+import { DomRunner, IDomRunnerArgs, IDomRunnerExec, IDomRunnerOptions } from './DomRunner';
 
-import { Scrap, Source, SourceData, SourceScrapper } from 'sourcescrapper-core';
+import { ISourceData, ISourceScrapper, RunnerScrapper, Scrap, Source } from 'sourcescrapper-core';
 
-export class SimpleDomScrapper extends SourceScrapper {
+export class SimpleDomScrapper extends RunnerScrapper
+    <ISourceData, IDomRunnerOptions, IDomRunnerArgs, IDomRunnerExec<ISourceData>, DomRunner<ISourceData>> {
     public static Name: string = 'simpledom';
     public static Domains: string[] = [];
     public static UrlPattern: RegExp = /.*/;
-    public static async scrap(url: string): Promise<Scrap<SourceData>> {
-        return new SimpleDomScrapper().scrap(url);
-    }
     public name: string =  SimpleDomScrapper.Name;
     public domains: string[] = SimpleDomScrapper.Domains;
     public urlPattern: RegExp = SimpleDomScrapper.UrlPattern;
-    protected async run(url: string): Promise<SourceData> {
-        return DomRunner.run<SourceData>(url, ({ dom }) => {
-            const data: SourceData = {
-                sources: []
-            };
-            const videos = dom.getElementsByTagName('video');
-            for (const video of videos) {
-                const vsrc = video.attrs.src;
-                if (vsrc)
+    protected async runWithArgs({ document }: IDomRunnerArgs): Promise<ISourceData> {
+        const data: ISourceData = {
+            sources: []
+        };
+        const videos = document.getElementsByTagName('video');
+        for (const video of videos) {
+            const vsrc = video.src;
+            if (vsrc)
+                data.sources.push(new Source({
+                    url: vsrc,
+                    type: video.getAttribute('type') || undefined
+                }));
+            const poster = video.poster;
+            if (poster)
+                data.poster = poster;
+            const sources = video.getElementsByTagName('source');
+            for (const source of sources) {
+                const ssrc = source.src;
+                if (ssrc)
                     data.sources.push(new Source({
-                        url: vsrc,
-                        type: video.attrs.type
+                        url: ssrc,
+                        type: video.getAttribute('type') || undefined
                     }));
-                const poster = video.attrs.poster;
-                if (poster)
-                    data.poster = poster;
-                const sources = video.getElementsByTagName('source');
-                for (const source of sources) {
-                    const ssrc = source.attrs.src;
-                    if (ssrc)
-                        data.sources.push(new Source({
-                            url: ssrc,
-                            type: source.attrs.type
-                        }));
-                }
             }
-            const titles = dom.getElementsByTagName('title');
-            if (titles.length >= 1)
-                data.title = titles[0].getText();
-            return Promise.resolve(data);
-        });
+        }
+        const titles = document.getElementsByTagName('title');
+        if (titles.length >= 1)
+            data.title = titles[0].innerText;
+        return Promise.resolve(data);
     }
 }
