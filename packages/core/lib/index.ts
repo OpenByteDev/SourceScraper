565e4a52375d11eb0d262eebfd04e0d27772d5ff
OpenByte <development.openbyte@gmail.com>
2018-08-01 23:19:42 +0200

    updated exports

--- packages/core/lib/index.ts
+++ packages/core/lib/index.ts
@@ -1,11 +1,8 @@
-import './extensions';
-
+export * from './Hoster';
+export * from './Runner';
+export * from './RunnerScrapper';
 export * from './Scrap';
 export * from './Scrapper';
 export * from './ScrapperArray';
-export * from './Hoster';
-export * from './HosterScrapper';
 export * from './Source';
-export * from './SourceScrapper';
-export * from './Runner';
 export * from './types';
