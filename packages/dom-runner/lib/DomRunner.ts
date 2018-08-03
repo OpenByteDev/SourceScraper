66f925696eacdd3b9ecac5a5a1d190aeaf7c26db
OpenbyteDev <development.openbyte@gmail.com>
2018-08-04 00:24:03 +0200

    applied changes of core to dom-runner

--- packages/dom-runner/lib/DomRunner.ts
+++ packages/dom-runner/lib/DomRunner.ts
@@ -1,5 +1,5 @@
 import { DOMWindow, FromUrlOptions, JSDOM } from 'jsdom';
-import { IRunner, IRunnerArgs, IRunnerExec, IRunnerOptions, Runner } from 'sourcescrapper-core';
+import { IRunner, IRunnerArgs, IRunnerOptions, Runner } from 'sourcescrapper-core';
 
 export interface IDomRunnerOptions extends IRunnerOptions {
     jsdomConfig?: FromUrlOptions;
@@ -11,22 +11,17 @@ export interface IDomRunnerArgs extends IRunnerArgs<IDomRunnerOptions> {
     window: DOMWindow;
 }
 
-export interface IDomRunnerExec<T> extends IRunnerExec<T, IDomRunnerOptions, IDomRunnerArgs> {
-    (args: IDomRunnerArgs): Promise<T>;
-}
+export interface IDomRunner<T> extends IRunner<T, IDomRunnerOptions, IDomRunnerArgs> { }
 
-export interface IDomRunner<T> extends IRunner<T, IDomRunnerOptions, IDomRunnerArgs, IDomRunnerExec<T>> { }
-
-export class DomRunner<T> extends Runner<T, IDomRunnerOptions, IDomRunnerArgs, IDomRunnerExec<T>>
-    implements IDomRunner<T> {
+export class DomRunner<T> extends Runner<T, IDomRunnerOptions, IDomRunnerArgs> implements IDomRunner<T> {
     public static DefaultOptions: IDomRunnerOptions = {
         jsdomConfig: {}
     };
     public defaultOptions: IDomRunnerOptions = DomRunner.DefaultOptions;
-    public constructor() {
-        super();
-    }
-    public async run(url: string, scrapper: IDomRunnerExec<T>, options?: IDomRunnerOptions): Promise<T> {
+    public async run(
+        url: string,
+        scrapper: (args: IDomRunnerArgs) => Promise<T>,
+        options?: IDomRunnerOptions): Promise<T> {
         const opt = this.getOptions(options);
         const jsdom = await JSDOM.fromURL(url, opt.jsdomConfig);
         return scrapper({
