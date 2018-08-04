7d878c7c8bf68eb47b7d8452b925a672dd80aa1c
OpenbyteDev <development.openbyte@gmail.com>
2018-08-05 01:29:16 +0200

    openload: applied api changes

--- packages/openload/lib/OpenloadScrapper.ts
+++ packages/openload/lib/OpenloadScrapper.ts
@@ -1,38 +1,42 @@
-import { Scrap, Source, SourceData, SourceScrapper } from 'sourcescrapper-core';
-import { PuppeteerRunner } from 'sourcescrapper-puppeteer-runner';
+import { ISourceData, Scrap, Source, SourceRunnerScrapper } from 'sourcescrapper-core';
+import { IPuppeteerRunnerArgs, PuppeteerRunner } from 'sourcescrapper-puppeteer-runner';
 
-export interface OpenloadSourceData extends SourceData {
+export interface IOpenloadSourceData extends ISourceData {
     streamurl: string;
 }
-export class OpenloadScrapper extends SourceScrapper<OpenloadSourceData> {
+
+export class OpenloadScrapper extends SourceRunnerScrapper<IOpenloadSourceData> {
     public static Name: string = 'openload';
     public static Domains: string[] = ['openload.co', 'oload.tv', 'oload.win'];
     public static UrlPattern: RegExp = /https?:\/\/(www\.)?(openload\.co|oload\.(?:tv|win))\/embed\/(\w+)/i;
-    public static async scrap(url: string): Promise<Scrap<OpenloadSourceData>> {
+    public static Runner: PuppeteerRunner<IOpenloadSourceData> = new PuppeteerRunner<IOpenloadSourceData>();
+    public static async scrap(url: string): Promise<Scrap<IOpenloadSourceData>> {
         return new OpenloadScrapper().scrap(url);
+    }
+    public static async scrapFromArgs(url: string, args: IPuppeteerRunnerArgs): Promise<Scrap<IOpenloadSourceData>> {
+        return new OpenloadScrapper().scrapFromArgs(url, args);
     }
     public name: string = OpenloadScrapper.Name;
     public domains: string[] = OpenloadScrapper.Domains;
     public urlPattern: RegExp = OpenloadScrapper.UrlPattern;
-    protected async run(url: string): Promise<OpenloadSourceData> {
-        return PuppeteerRunner.run(url, async ({ page }) => {
-            const streamurl = await page.$eval(
-                '[id*=stream], div[style*="display:none"] p:last-of-type',
-                e => e.innerText);
-            const title = await page.$eval(
-                'meta[name="description"], meta[name="og:title"], meta[name="twitter:title"]',
-                e => e.content).catch(e => undefined);
-            const thumb = await page.$eval(
-                'meta[name="og:image"], meta[name="twitter:image"]',
-                e => e.content).catch(e => undefined);
-            return {
-                sources: [new Source({
-                    url: `https://openload.co/stream/${await streamurl}?mime=true`
-                })],
-                title,
-                poster: thumb,
-                streamurl
-            };
-        });
+    public runner: PuppeteerRunner<IOpenloadSourceData> = OpenloadScrapper.Runner;
+    protected async runWithArgs({ page }: IPuppeteerRunnerArgs): Promise<IOpenloadSourceData> {
+        const streamurl = await page.$eval(
+            '[id*=stream], div[style*="display:none"] p:last-of-type',
+            e => e.innerHTML);
+        const title = await page.$eval(
+            'meta[name="description"], meta[name="og:title"], meta[name="twitter:title"]',
+            e => e.innerHTML).catch(() => undefined);
+        const thumb = await page.$eval(
+            'meta[name="og:image"], meta[name="twitter:image"]',
+            e => e.innerHTML).catch(() => undefined);
+        return {
+            sources: [new Source({
+                url: `https://openload.co/stream/${await streamurl}?mime=true`
+            })],
+            title,
+            poster: thumb,
+            streamurl
+        };
     }
 }
