f2c0a34968a8e048e3a1154f568fe97380d65ed0
OpenbyteDev <development.openbyte@gmail.com>
2018-08-05 00:19:33 +0200

    masteranime: fixed linting errors

--- packages/masteranime/lib/MasterAnimeScrapper.ts
+++ packages/masteranime/lib/MasterAnimeScrapper.ts
@@ -1,7 +1,7 @@
 import { MasterAnimeHoster } from './MasterAnimeHoster';
 
 import { MasterAnimeAPI } from 'masteranime-api';
-import { Scrap, HosterScrapper, IHosterData } from 'sourcescrapper-core';
+import { HosterScrapper, IHosterData, Scrap } from 'sourcescrapper-core';
 
 export interface IMasterAnimeHosterData extends IHosterData {
     data: MasterAnimeAPI;
@@ -11,7 +11,7 @@ export class MasterAnimeScrapper extends HosterScrapper<IMasterAnimeHosterData> 
     public static Name: string = 'masteranime';
     public static Domains: string[] = ['masterani.me'];
     public static UrlPattern: RegExp = /https?:\/\/(www\.)?masterani\.me\/anime\/watch\/(\d+-(?:\w+-)+\w+)\/(\d+)/i;
-    public static scrap(url: string): Promise<Scrap<IMasterAnimeHosterData>> {
+    public static async scrap(url: string): Promise<Scrap<IMasterAnimeHosterData>> {
         return new MasterAnimeScrapper().scrap(url);
     }
     public name: string = MasterAnimeScrapper.Name;
