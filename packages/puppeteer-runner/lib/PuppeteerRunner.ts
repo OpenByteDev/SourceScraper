fc3cace85182cc441c236e17e590ffdff8ac724b
OpenbyteDev <development.openbyte@gmail.com>
2018-08-04 14:38:56 +0200

    puppeteer-runner: applied changed from core

--- packages/puppeteer-runner/lib/PuppeteerRunner.ts
+++ packages/puppeteer-runner/lib/PuppeteerRunner.ts
@@ -1,44 +1,53 @@
-import { Runner, RunnerArgs, RunnerOptions } from 'sourcescrapper-core';
+import { IRunner, IRunnerArgs, IRunnerOptions, Runner } from 'sourcescrapper-core';
 
 import { Browser, launch as puppeteerLaunch, LaunchOptions, NavigationOptions, Page, Request } from 'puppeteer';
 
-export interface PuppeteerRunnerArgs<T> extends RunnerArgs<T> {
+export interface IPuppeteerRunnerOptions extends IRunnerOptions {
+    puppeteerConfig?: LaunchOptions;
+    requestInterceptors?: IRequestInterceptor[];
+    init?: IInitializer;
+    navigationOptions?: NavigationOptions;
+}
+
+export interface IPuppeteerRunnerArgs extends IRunnerArgs<IPuppeteerRunnerOptions> {
     browser: Browser;
     page: Page;
 }
-export type PuppeteerRunnerExec<T> = (PuppeteerRunnerArgs) => Promise<T>;
-export interface PuppeteerRunnerOptions extends RunnerOptions {
-    puppeteerConfig?: LaunchOptions;
-    requestInterceptors?: RequestInterceptor[];
-    init?: Initializer;
-    navigationOptions?: NavigationOptions;
-}
-export type RequestInterceptor = (RequestInterceptorArgs) => boolean;
-export interface RequestInterceptorArgs {
+
+export interface IRequestInterceptorArgs {
     resourceType: string;
     url: string;
     page: Page;
     browser: Browser;
+    request: Request;
 }
-export type Initializer = (InitializerArgs) => void;
-export interface InitializerArgs {
+
+export type IRequestInterceptor = (args: IRequestInterceptorArgs) => boolean;
+
+export interface IInitializerArgs {
     page: Page;
     browser: Browser;
 }
-export class PuppeteerRunner<T> extends Runner<T, PuppeteerRunnerExec<T>, PuppeteerRunnerOptions> {
-    // tslint:disable-next-line
-    public static async run<T>(url: string, scrapper: PuppeteerRunnerExec<T>, options?: PuppeteerRunnerOptions): Promise<T> {
-        return new PuppeteerRunner<T>().run(url, scrapper, options);
-    }
-    public defaultOptions: PuppeteerRunnerOptions = {
+
+export type IInitializer = (args: IInitializerArgs) => void;
+
+export interface IPuppeteerRunner<T> extends IRunner<T, IPuppeteerRunnerOptions, IPuppeteerRunnerArgs> { }
+
+export class PuppeteerRunner<T> extends Runner<T, IPuppeteerRunnerOptions, IPuppeteerRunnerArgs>
+    implements IPuppeteerRunner<T> {
+    public static DefaultOptions: IPuppeteerRunnerOptions = {
         puppeteerConfig: {
             headless: false
         },
         requestInterceptors: [
-            ({ request }) => request.resourceType === 'font'
+            ({ resourceType }: IRequestInterceptorArgs) => resourceType === 'font'
         ]
     };
-    public async run(url: string, scrapper: PuppeteerRunnerExec<T>, options?: PuppeteerRunnerOptions): Promise<T> {
+    public defaultOptions: IPuppeteerRunnerOptions = PuppeteerRunner.DefaultOptions;
+    public async run(
+        url: string,
+        scrapper: (args: IPuppeteerRunnerArgs) => Promise<T>,
+        options?: IPuppeteerRunnerOptions): Promise<T> {
         const { puppeteerConfig, requestInterceptors, init, navigationOptions } = this.getOptions(options);
 
         const browser = await puppeteerLaunch(puppeteerConfig);
@@ -50,7 +59,7 @@ export class PuppeteerRunner<T> extends Runner<T, PuppeteerRunnerExec<T>, Puppet
         await page.setRequestInterception(requestInterceptionEnabled);
         if (requestInterceptionEnabled)
             page.on('request', (request: Request) => {
-                const block = (requestInterceptors as RequestInterceptor[]).some(e => e({
+                const block = (requestInterceptors as IRequestInterceptor[]).some(e => e({
                     browser,
                     page,
                     request,
@@ -75,9 +84,9 @@ export class PuppeteerRunner<T> extends Runner<T, PuppeteerRunnerExec<T>, Puppet
 
         try {
             return await scrapper({
+                url,
                 browser,
-                page,
-                url
+                page
             });
         } finally {
             await browser.close();
--- packages/puppeteer-runner/lib/SimplePuppeteerScrapper.ts
+++ packages/puppeteer-runner/lib/SimplePuppeteerScrapper.ts
@@ -1,49 +1,53 @@
-import { PuppeteerRunner } from './PuppeteerRunner';
+import { IPuppeteerRunnerArgs, IPuppeteerRunnerOptions, PuppeteerRunner } from './PuppeteerRunner';
 
-import { Scrap, Source, SourceData, SourceScrapper } from 'sourcescrapper-core';
+import { ISourceData, ISourceScrapper, Source, SourceRunnerScrapper } from 'sourcescrapper-core';
 
-export class SimplePuppeteerScrapper extends SourceScrapper {
+export class SimplePuppeteerScrapper
+    extends SourceRunnerScrapper<
+        ISourceData,
+        IPuppeteerRunnerOptions,
+        IPuppeteerRunnerArgs,
+        PuppeteerRunner<ISourceData>>
+    implements ISourceScrapper {
     public static Name: string = 'puppeteer';
     public static Domains: string[] = [];
-    public static UrlPattern = /.*/;
-    public static async scrap(url: string): Promise<Scrap<SourceData>> {
-        return new SimplePuppeteerScrapper().scrap(url);
-    }
-    public name: string = 'puppeteer';
-    public domains: string[] = [];
-    public urlPattern = /.*/;
-    protected async run(url: string): Promise<SourceData> {
-        return PuppeteerRunner.run<SourceData>(url, ({ page }) => {
-            return page.evaluate(() => {
-                const data: SourceData = {
-                    sources: []
-                };
-                const videos = document.getElementsByTagName('video');
-                for (const video of videos) {
-                    const vsrc = video.src;
-                    if (vsrc)
+    public static UrlPattern: RegExp = /.*/;
+    public static Runner: PuppeteerRunner<ISourceData> = new PuppeteerRunner<ISourceData>();
+    public name: string = SimplePuppeteerScrapper.Name;
+    public domains: string[] = SimplePuppeteerScrapper.Domains;
+    public urlPattern: RegExp = SimplePuppeteerScrapper.UrlPattern;
+    public runner: PuppeteerRunner<ISourceData> = SimplePuppeteerScrapper.Runner;
+    protected async runWithArgs({ page }: IPuppeteerRunnerArgs): Promise<ISourceData> {
+        // tslint:disable-next-line no-shadowed-variable
+        return page.evaluate(async Source => {
+            const data: ISourceData = {
+                sources: []
+            };
+            const videos = document.getElementsByTagName('video');
+            for (const video of videos) {
+                const vsrc = video.src;
+                if (vsrc)
+                    data.sources.push(new Source({
+                        url: vsrc,
+                        type: video.getAttribute('type') || undefined
+                    }));
+                const poster = video.poster;
+                if (poster)
+                    data.poster = poster;
+                const sources = video.getElementsByTagName('source');
+                for (const source of sources) {
+                    const ssrc = source.src;
+                    if (ssrc)
                         data.sources.push(new Source({
-                            url: vsrc,
-                            type: video.getAttribute('type') || undefined
+                            url: ssrc,
+                            type: source.getAttribute('type') || undefined
                         }));
-                    const poster = video.poster;
-                    if (poster)
-                        data.poster = poster;
-                    const sources = video.getElementsByTagName('source');
-                    for (const source of sources) {
-                        const ssrc = source.src;
-                        if (ssrc)
-                            data.sources.push(new Source({
-                                url: ssrc,
-                                type: source.getAttribute('type') || undefined
-                            }));
-                    }
                 }
-                const titles = document.getElementsByTagName('title');
-                if (titles.length >= 1)
-                    data.title = titles[0].innerText;
-                return Promise.resolve(data);
-            });
-        });
+            }
+            const titles = document.getElementsByTagName('title');
+            if (titles.length >= 1)
+                data.title = titles[0].innerText;
+            return Promise.resolve(data);
+        }, Source);
     }
 }
