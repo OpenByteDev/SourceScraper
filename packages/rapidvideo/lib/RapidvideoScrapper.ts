e7567779a69c4e4c280110818de2ef67571413be
OpenbyteDev <development.openbyte@gmail.com>
2018-08-05 01:30:39 +0200

    rapidvideo: removed unnecessary method

--- packages/rapidvideo/lib/RapidvideoScrapper.ts
+++ packages/rapidvideo/lib/RapidvideoScrapper.ts
@@ -1,13 +1,9 @@
-import { Scrap, SourceData } from 'sourcescrapper-core';
 import { SimpleDomScrapper } from 'sourcescrapper-dom-runner';
 
 export class RapidvideoScrapper extends SimpleDomScrapper {
     public static Name: string = 'rapidvideo';
     public static Domains: string[] = ['rapidvideo.com'];
     public static UrlPattern: RegExp = /https?:\/\/(www\.)?rapidvideo\.com\/e\/(\w+)/i;
-    public static async scrap(url: string): Promise<Scrap<SourceData>> {
-        return new RapidvideoScrapper().scrap(url);
-    }
     public name: string = RapidvideoScrapper.Name;
     public domains: string[] = RapidvideoScrapper.Domains;
     public urlPattern: RegExp = RapidvideoScrapper.UrlPattern;
