a8831d9d67424710b6f6ce826bf977d41bb4cea7
OpenbyteDev <development.openbyte@gmail.com>
2018-08-04 15:47:26 +0200

    dom-runner: now using static run method in test

--- packages/dom-runner/test/DomRunner.spec.ts
+++ packages/dom-runner/test/DomRunner.spec.ts
@@ -1,4 +1,4 @@
-import { DomRunner, IDomRunnerArgs } from '../lib';
+import { DomRunner } from '../lib';
 
 import chai = require('chai');
 import { JSDOM } from 'jsdom';
@@ -8,8 +8,7 @@ chai.should();
 describe('DomRunner', () => {
     it('should provide valid args', async () => {
         const url = 'http://tekeye.uk/html/html5-video-test-page';
-        const runner = new DomRunner<IDomRunnerArgs>();
-        const args = await runner.run(url, a => Promise.resolve(a));
+        const args = await DomRunner.run(url, a => Promise.resolve(a));
         args.should.have.property('jsdom').that.is.instanceOf(JSDOM);
         args.should.have.property('document').that.equals(args.jsdom.window.document);
         args.should.have.property('window').that.equals(args.jsdom.window);
