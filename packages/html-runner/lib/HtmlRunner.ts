1f94ccc9d1371c9bf0aa4f4c08c76318f23d668e
OpenbyteDev <development.openbyte@gmail.com>
2018-08-04 16:33:08 +0200

    html-runner: added static run method to HtmlRunner

--- packages/html-runner/lib/HtmlRunner.ts
+++ packages/html-runner/lib/HtmlRunner.ts
@@ -16,6 +16,12 @@ export class HtmlRunner<T> extends Runner<T, IHtmlRunnerOptions, IHtmlRunnerArgs
     public static DefaultOptions: IHtmlRunnerOptions = {
         axiosConfig: {}
     };
+    public static async run<T>(
+        url: string,
+        scrapper: (args: IHtmlRunnerArgs) => Promise<T>,
+        options?: IHtmlRunnerOptions): Promise<T> {
+        return new HtmlRunner<T>().run(url, scrapper, options);
+    }
     public defaultOptions: IHtmlRunnerOptions = HtmlRunner.DefaultOptions;
     public async run(
         url: string,
