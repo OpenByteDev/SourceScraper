b639214359158f9f53ad04e20b0834db228f43be
OpenbyteDev <development.openbyte@gmail.com>
2018-08-04 21:38:16 +0200

    masteranime: added static scrap method

--- packages/masteranime/lib/MasterAnimeScrapper.ts
+++ packages/masteranime/lib/MasterAnimeScrapper.ts
@@ -1,7 +1,7 @@
 import { MasterAnimeHoster } from './MasterAnimeHoster';
 
 import { MasterAnimeAPI } from 'masteranime-api';
-import { HosterScrapper, IHosterData } from 'sourcescrapper-core';
+import { Scrap, HosterScrapper, IHosterData } from 'sourcescrapper-core';
 
 export interface IMasterAnimeHosterData extends IHosterData {
     data: MasterAnimeAPI;
@@ -11,6 +11,9 @@ export class MasterAnimeScrapper extends HosterScrapper<IMasterAnimeHosterData> 
     public static Name: string = 'masteranime';
     public static Domains: string[] = ['masterani.me'];
     public static UrlPattern: RegExp = /https?:\/\/(www\.)?masterani\.me\/anime\/watch\/(\d+-(?:\w+-)+\w+)\/(\d+)/i;
+    public static scrap(url: string): Promise<Scrap<IMasterAnimeHosterData>> {
+        return new MasterAnimeScrapper().scrap(url);
+    }
     public name: string = MasterAnimeScrapper.Name;
     public domains: string[] = MasterAnimeScrapper.Domains;
     public urlPattern: RegExp = MasterAnimeScrapper.UrlPattern;
