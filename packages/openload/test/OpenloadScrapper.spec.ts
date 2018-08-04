a9f7a73bf1b0722adf58bea4cf30106a05b7e0ae
OpenbyteDev <development.openbyte@gmail.com>
2018-08-05 01:29:38 +0200

    openload: updated dependencies; added tests

---
+++ packages/openload/test/OpenloadScrapper.spec.ts
@@ -0,0 +1,20 @@
+import { OpenloadScrapper } from '../lib';
+
+import { ISourceData } from 'sourcescrapper-core';
+
+import chai = require('chai');
+import 'mocha';
+chai.should();
+
+describe('OpenloadScrapper', () => {
+    it('should be able to scrap a video from a test page', async () => {
+        const url = 'https://openload.co/embed/t0jz0bXYJbY';
+        const scrap = await OpenloadScrapper.scrap(url);
+        scrap.should.have.property('success').that.is.a('boolean').and.that.is.true;
+        scrap.should.have.property('data').that.is.an('object').and.that.is.not.undefined;
+        const data = scrap.data as ISourceData;
+        data.should.have.property('sources').that.is.an('array');
+        data.sources.length.should.be.greaterThan(0);
+        data.sources.forEach(h => h.should.have.property('url').that.is.a('string'));
+    });
+});
