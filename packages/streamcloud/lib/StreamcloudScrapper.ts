535e233f28de769210dd8d23dc1bce0f56fa555d
OpenbyteDev <development.openbyte@gmail.com>
2018-08-05 01:33:09 +0200

    streamcloud: applied api changes

--- packages/streamcloud/lib/StreamcloudScrapper.ts
+++ packages/streamcloud/lib/StreamcloudScrapper.ts
@@ -1,10 +1,11 @@
-import { integer, Scrap, Source, SourceData, SourceScrapper, uri } from 'sourcescrapper-core';
+import { integer, ISourceData, Scrap, Source, SourceScrapper, uri } from 'sourcescrapper-core';
 import { HtmlRunner } from 'sourcescrapper-html-runner';
 
 import removeNewlines = require('newline-remove');
 import queryString from 'querystring';
+import execAll = require('regexp.execall');
 
-export interface JWPlayerSetupData {
+export interface IJWPlayerSetupData {
     provider: string;
     flashplayer: uri;
     file: uri;
@@ -15,20 +16,22 @@ export interface JWPlayerSetupData {
     aboutlink: uri;
     startparam: string;
 }
-export interface StreamcloudSourceData extends SourceData {
-    setupData: JWPlayerSetupData;
+
+export interface IStreamcloudSourceData extends ISourceData {
+    setupData: IJWPlayerSetupData;
 }
-export class StreamcloudScrapper extends SourceScrapper<StreamcloudSourceData> {
+
+export class StreamcloudScrapper extends SourceScrapper<IStreamcloudSourceData> {
     public static Name: string = 'streamcloud';
     public static Domains: string[] = ['streamcloud.eu'];
     public static UrlPattern: RegExp = /https?:\/\/streamcloud\.eu\/(\w+)\/(.+)/i;
-    public static async scrap(url: string): Promise<Scrap<StreamcloudSourceData>> {
+    public static async scrap(url: string): Promise<Scrap<IStreamcloudSourceData>> {
         return new StreamcloudScrapper().scrap(url);
     }
     public name: string = StreamcloudScrapper.Name;
     public domains: string[] = StreamcloudScrapper.Domains;
     public urlPattern: RegExp = StreamcloudScrapper.UrlPattern;
-    protected async run(url: uri): Promise<StreamcloudSourceData> {
+    protected async run(url: uri): Promise<IStreamcloudSourceData> {
         const dataRegex = /\/([^\/.]+)\/(.*)\.[^.]+/i;
         const data = dataRegex.exec(url);
         if (data === null || data.length <= 2)
@@ -39,7 +42,7 @@ export class StreamcloudScrapper extends SourceScrapper<StreamcloudSourceData> {
             return Promise.reject(null);
         const html = await HtmlRunner.run(
             url,
-            ({ html: _html }) => Promise.resolve(removeNewlines(_html)),
+            async ({ html: _html }) => Promise.resolve(removeNewlines(_html)),
             {
                 axiosConfig: {
                     method: 'post',
@@ -69,8 +72,8 @@ export class StreamcloudScrapper extends SourceScrapper<StreamcloudSourceData> {
         if (!configData || configData.length < 1)
             return Promise.reject(null);
         const configRAW = configData[1].replace(tabRegex, '');
-        const propData = propRegex.execAll(configRAW) as any[];
-        const props: JWPlayerSetupData = propData
+        const propData = execAll(propRegex, configRAW) as any[];
+        const props: IJWPlayerSetupData = propData
             .filter(e => e !== null && e.length >= 2)
             .reduce((a, [, key, value]) => a[key] = value, {});
         return {
