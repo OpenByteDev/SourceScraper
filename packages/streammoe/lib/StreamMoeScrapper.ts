82159ba5061b7654c4f73a832138fa10c12c515b
OpenByte <development.openbyte@gmail.com>
2018-08-05 21:15:47 +0200

    added streammoe package

---
+++ packages/streammoe/lib/StreamMoeScrapper.ts
@@ -0,0 +1,43 @@
+import { ISourceData, Scrap, SourceRunnerScrapper } from 'sourcescrapper-core';
+import { SimpleDomScrapper } from 'sourcescrapper-dom-runner';
+import { HtmlRunner, IHtmlRunnerArgs } from 'sourcescrapper-html-runner';
+
+export interface IStreamMoeSourceData extends ISourceData {
+    decodedPage: string;
+}
+
+export class StreamMoeScrapper extends SourceRunnerScrapper<IStreamMoeSourceData> {
+    public static Name: string = 'streammoe';
+    public static Domains: string[] = ['stream.moe'];
+    public static UrlPattern: RegExp = /https?:\/\/(www\.)?stream\.moe\/embed2\/[0-9a-zA-Z]+/i;
+    public static Runner: HtmlRunner<IStreamMoeSourceData> = new HtmlRunner<IStreamMoeSourceData>();
+    public static async scrap(url: string): Promise<Scrap<IStreamMoeSourceData>> {
+        return new StreamMoeScrapper().scrap(url);
+    }
+    public static async scrapFromArgs(args: IHtmlRunnerArgs): Promise<Scrap<IStreamMoeSourceData>> {
+        return new StreamMoeScrapper().scrapFromArgs(args);
+    }
+    public name: string = StreamMoeScrapper.Name;
+    public domains: string[] = StreamMoeScrapper.Domains;
+    public urlPattern: RegExp = StreamMoeScrapper.UrlPattern;
+    public runner: HtmlRunner<IStreamMoeSourceData> = StreamMoeScrapper.Runner;
+    protected async runWithArgs({ html, url }: IHtmlRunnerArgs): Promise<IStreamMoeSourceData> {
+        const encodedDataRegex = /atob\((['"])(.*?)\1\)/i;
+        const encodedData = encodedDataRegex.exec(html);
+        if (encodedData === null || encodedData.length < 3)
+            return Promise.reject(null);
+        const encoded = encodedData[2];
+        const decoded = Buffer.from(encoded, 'base64').toString('ascii');
+        const scrap = await SimpleDomScrapper.scrapFromHtml(url, decoded);
+        if (!scrap.success || typeof scrap.data === 'undefined')
+            return Promise.reject(null);
+        const titleRegex = /<title[^>]*>\s*(?:\[.*?])\s*(.*?)\s*(?:\[.*?]).*?<\/title>/i;
+        const titleData = titleRegex.exec(html);
+        const title = titleData !== null && titleData.length >= 1 ? titleData[0] : undefined;
+        return {
+            ...scrap.data,
+            title,
+            decodedPage: decoded,
+        };
+    }
+}
