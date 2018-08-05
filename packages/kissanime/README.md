3913afd23f9d0ea93108145549dc3a8df9dd32da
OpenByte <development.openbyte@gmail.com>
2018-08-05 23:02:04 +0200

    added kissanime package

---
+++ packages/kissanime/README.md
@@ -0,0 +1,8 @@
+# Kissanime Scrapper
+
+[![Build Status](https://travis-ci.org/OpenByteDev/SourceScrapper.svg?branch=master)](https://travis-ci.org/OpenByteDev/SourceScrapper)
+[![npm version](https://badge.fury.io/js/kissanime-scrapper.svg)](https://www.npmjs.com/package/kissanime-scrapper)
+[![License](https://img.shields.io/github/license/mashape/apistatus.svg)](https://opensource.org/licenses/MIT)
+[![Doge](https://img.shields.io/badge/doge-wow-yellow.svg)]()
+
+A scrapper for Kissanime
---
+++ packages/kissanime/test/KissanimeScrapper.spec.ts
@@ -0,0 +1,20 @@
+import { KissanimeScrapper } from '../lib';
+
+import { IHosterData } from 'sourcescrapper-core';
+
+import chai = require('chai');
+import 'mocha';
+chai.should();
+
+describe('KissanimeScrapper', () => {
+    it('should be able to scrap a video from a test page', async () => {
+        const url = 'http://kissanime.ru/Anime/Shingeki-no-Kyojin-Season-3/Episode-001?id=148021';
+        const scrap = await KissanimeScrapper.scrap(url);
+        scrap.should.have.property('success').that.is.a('boolean').and.that.is.true;
+        scrap.should.have.property('data').that.is.an('object').and.that.is.not.undefined;
+        const data = scrap.data as IHosterData;
+        data.should.have.property('hosters').that.is.an('array');
+        data.hosters.length.should.be.greaterThan(0);
+        data.hosters.forEach(h => h.should.have.property('url').that.is.a('string'));
+    });
+});
