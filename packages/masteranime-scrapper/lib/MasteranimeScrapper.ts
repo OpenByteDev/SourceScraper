dc4fec9047d3fbfd35e114d68aaaba4add69389a
OpenByte <development.openbyte@gmail.com>
2018-08-20 20:22:10 +0200

    masteranime: Fix scrapping

--- packages/masteranime-scrapper/lib/MasteranimeScrapper.ts
+++ packages/masteranime-scrapper/lib/MasteranimeScrapper.ts
@@ -5,59 +5,10 @@ import { HosterRunnerScrapper, IHosterData, IRunnerScrapperOptions, Scrap } from
 import { HtmlRunner, IHtmlRunnerArgs, IHtmlRunnerOptions } from 'sourcescrapper-html-runner';
 
 export interface IMasteranimeHosterData extends IHosterData {
-    data: IEpisodeDetailed;
+    mirrors: IMirror[];
     hosters: MasteranimeHoster[];
 }
 
-export interface IEpisodeDetailed {
-    anime: {
-        info: IShortAnimeInfo;
-        poster: string;
-        episodes: {
-            current: ICurrentEpisode;
-            next: IEpisode | null;
-            prev: IEpisode | null;
-        };
-    };
-    mirrors: IMirror[];
-    auto_update: number[];
-}
-export interface IEpisode {
-    id: number;
-    episode: string;
-}
-export interface ICurrentEpisode extends IEpisode {
-    subbed: number;
-    dubbed: number;
-    type: number;
-    title: string | null;
-    duration: number | null;
-    created_at: string;
-    tvdb_id: number | null;
-    description: string | null;
-    aired: string | null;
-    users: IUser[] | null;
-    extra_viewers: number;
-}
-export interface IUser {
-    id: number;
-    name: string;
-    last_time_seen: string;
-    is_online: boolean;
-    avatar: IAvatar | null;
-}
-export interface IAvatar {
-    id: string;
-    path: string;
-    extension: string;
-    file: string;
-}
-export interface IShortAnimeInfo {
-    id: number;
-    title: string;
-    slug: string;
-    episode_length: number;
-}
 export interface IMirror {
     id: number;
     host_id: number;
@@ -93,16 +44,21 @@ export class MasteranimeScrapper extends HosterRunnerScrapper<IMasteranimeHoster
     public defaultOptions: IMasteranimeScrapperOptions = MasteranimeScrapper.DefaultOptions;
     public runner: HtmlRunner<IMasteranimeHosterData>  = MasteranimeScrapper.Runner;
     protected async execWithArgs({ html }: IHtmlRunnerArgs): Promise<IMasteranimeHosterData> {
-        const argsRegex = /<script[^>]*>\s*(?:(?:var|let|const)\s*)?args\s*=\s*({.*?})\s*(;\s*)?<\/script>/i;
-        const argsData = argsRegex.exec(html);
-        if (argsData === null || argsData.length < 2)
-            return Promise.reject(new Error('Unable to find data'));
-        const argsString = argsData[1];
-        const data = jsonic(argsString) as IEpisodeDetailed;
+        const mirrorsRegex = /mirrors='(.+?)'/i;
+        const mirrorsData = mirrorsRegex.exec(html);
+        if (mirrorsData === null || mirrorsData.length < 2)
+            return Promise.reject(new Error('Unable to find mirrors'));
+        const argsString = mirrorsData[1];
+        const mirrors = jsonic(argsString) as IMirror[];
+
+        const titleRegex = /<img class="cover".*?alt="(.*?)"/i;
+        const titleData = titleRegex.exec(html);
+        const title = titleData !== null && titleData.length >= 2 ? titleData[1] : undefined;
+
         return {
-            data,
-            title: data.anime.info.title,
-            hosters: data.mirrors.map(e => new MasteranimeHoster({
+            mirrors,
+            title,
+            hosters: mirrors.map(e => new MasteranimeHoster({
                 name: e.host.name,
                 quality: e.quality,
                 url:
