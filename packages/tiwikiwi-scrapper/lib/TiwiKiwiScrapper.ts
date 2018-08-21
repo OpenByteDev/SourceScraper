1d6c64478f3c3c5b2bdb51e52cb39ea887daa5a3
OpenByte <development.openbyte@gmail.com>
2018-08-21 23:22:10 +0200

    tiwikiwi-scrapper: Fix linting errors

--- packages/tiwikiwi-scrapper/lib/TiwiKiwiScrapper.ts
+++ packages/tiwikiwi-scrapper/lib/TiwiKiwiScrapper.ts
@@ -1,8 +1,9 @@
-import { SimpleJWPlayerScrapper, IJWPlayerScrapperOptions } from 'sourcescrapper-jwplayer-runner';
+import { IJWPlayerScrapperOptions, SimpleJWPlayerScrapper } from 'sourcescrapper-jwplayer-runner';
 
 export class TiwiKiwiScrapper extends SimpleJWPlayerScrapper {
     public static Name: string = 'tiwikiwi';
     public static Domains: string[] = ['tiwi.kiwi'];
-    public static UrlPattern: RegExp = /(?:(?:https?:)?\/\/)?(?:[^.]+\.)?tiwi\.kiwi\/(embed-([0-9a-zA-Z]+)\.html|([0-9a-zA-Z]+))/i;
+    public static UrlPattern: RegExp =
+        /(?:(?:https?:)?\/\/)?(?:[^.]+\.)?tiwi\.kiwi\/(embed-([0-9a-zA-Z]+)\.html|([0-9a-zA-Z]+))/i;
     public static DefaultOptions: IJWPlayerScrapperOptions = {};
 }
