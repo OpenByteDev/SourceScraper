329c6caee8710cd4c9c60871e6e83790dcec6541
OpenbyteDev <development.openbyte@gmail.com>
2018-08-31 02:40:53 +0200

    vidzi-scraper: Fix urlPattern

--- packages/vidzi-scraper/lib/VidziScraper.ts
+++ packages/vidzi-scraper/lib/VidziScraper.ts
@@ -3,5 +3,5 @@ import { SimpleJWPlayerScraper } from 'source-scraper-jwplayer-runner';
 export class VidziScraper extends SimpleJWPlayerScraper {
     public name: string = 'vidzi';
     public domains: string[] = ['vidzi.tv', 'vidzi.nu', 'vidzi.nu', 'vidzi.online'];
-    public urlPattern: RegExp = /(?:(?:https?:)?\/\/)?(?:[^.]+\.)?vidzi\.(?:tv|online|nu|si)\/(\w+)\.html/i;
+    public urlPattern: RegExp = /(?:(?:https?:)?\/\/)?(?:[^.]+\.)?vidzi\.(?:tv|online|nu|si)\/(\w+)(\.html)?/i;
 }
