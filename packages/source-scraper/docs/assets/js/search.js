f816119e4dbd335ddd6adf5f650f3efb51226205
OpenbyteDev <development.openbyte@gmail.com>
2018-08-31 17:02:13 +0200

    Generate docs

---
+++ packages/source-scraper/docs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"32":"Variable"},"rows":[{"id":0,"kind":32,"name":"source","url":"globals.html#source","classes":"tsd-kind-variable"},{"id":1,"kind":32,"name":"hoster","url":"globals.html#hoster","classes":"tsd-kind-variable"},{"id":2,"kind":32,"name":"all","url":"globals.html#all","classes":"tsd-kind-variable"}]};
