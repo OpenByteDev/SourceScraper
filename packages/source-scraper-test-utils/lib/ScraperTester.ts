6110933ad3a15a900b6216f70b5a2054fb57a6ae
OpenbyteDev <development.openbyte@gmail.com>
2018-09-02 01:09:46 +0200

    source-scraper-test-utils: Update scraper type detection

--- packages/source-scraper-test-utils/lib/ScraperTester.ts
+++ packages/source-scraper-test-utils/lib/ScraperTester.ts
@@ -1,9 +1,7 @@
 import { IStatic } from './IStatic';
 import { Tester } from './Tester';
 
-import {
-    HosterScraper, IHosterData, IScraperOptions, ISourceData, Scraper, SourceScraper
-} from 'source-scraper-core';
+import { IHosterData, IScraperOptions, ISourceData, Scraper } from 'source-scraper-core';
 
 import chai = require('chai');
 import 'mocha';
@@ -38,13 +36,14 @@ export class ScraperTester<T, SO extends IScraperOptions, S extends Scraper<T, S
                 const scrap = await this.scraper.scrap(url, options);
                 scrap.should.have.property('success').that.is.true;
                 scrap.should.have.property('data').that.is.an('object');
-                if (this.scraper instanceof SourceScraper) {
-                    const data = scrap.data as any as ISourceData;
+                let data = scrap.data as any;
+                if ('sources' in data) {
+                    data = data as ISourceData;
                     data.should.have.property('sources').that.is.an('array');
                     data.sources.length.should.be.greaterThan(0);
                     data.sources.forEach(s => s.should.have.property('url').that.is.a('string'));
-                } else if (this.scraper instanceof HosterScraper) {
-                    const data = scrap.data as any as IHosterData;
+                } else if ('hosters' in data) {
+                    data = data as IHosterData;
                     data.should.have.property('hosters').that.is.an('array');
                     data.hosters.length.should.be.greaterThan(0);
                     data.hosters.forEach(s => s.should.have.property('url').that.is.a('string'));
