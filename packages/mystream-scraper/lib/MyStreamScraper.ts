39c0a129bf5a6c9d7c6287ec924c7780e827939d
OpenByte <development.openbyte@gmail.com>
2018-10-28 22:39:05 +0100

    mystream-scraper: Fixes #7
    source tag is now dynamically generated by javascript obfuscated using aaencode

--- packages/mystream-scraper/lib/MyStreamScraper.ts
+++ packages/mystream-scraper/lib/MyStreamScraper.ts
@@ -1,6 +1,6 @@
-import { SimpleDomScraper } from 'source-scraper-dom-runner';
+import { SimplePuppeteerScraper } from 'source-scraper-puppeteer-runner';
 
-export class MyStreamScraper extends SimpleDomScraper {
+export class MyStreamScraper extends SimplePuppeteerScraper {
     public name: string = 'mystream';
     public domains: string[] = ['mystream.to'];
     public urlPattern: RegExp = /(?:(?:https?:)?\/\/)?embed\.mystream\.to\/(\w+)/i;
--- packages/mystream-scraper/test/MyStreamScraper.spec.ts
+++ packages/mystream-scraper/test/MyStreamScraper.spec.ts
@@ -2,7 +2,12 @@ import { MyStreamScraper } from '../lib';
 
 import { ScraperTester } from 'source-scraper-test-utils';
 
-const urls = ['https://embed.mystream.to/8g8peyzkqbll'];
+const urls = [
+    'https://embed.mystream.to/8g8peyzkqbll',
+    'https://embed.mystream.to/wr35j4n3bh4b',
+    'https://embed.mystream.to/eidkwo3kl05f',
+    'https://embed.mystream.to/61n84ozx4fa7'
+];
 ScraperTester.fromStatic(MyStreamScraper)
     .testUrlDetection(urls)
     .testScraping(urls)
