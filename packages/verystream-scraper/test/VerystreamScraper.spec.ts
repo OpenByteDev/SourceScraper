c3aee62185d56bb62ce20f9ced676eba4f6b7cb5
OpenByte <development.openbyte@gmail.com>
2019-05-16 18:19:26 +0200

    Update package.json; Replace testing url

--- packages/verystream-scraper/test/VerystreamScraper.spec.ts
+++ packages/verystream-scraper/test/VerystreamScraper.spec.ts
@@ -2,9 +2,9 @@ import { VerystreamScraper } from '../lib';
 
 import { ScraperTester } from 'source-scraper-test-utils';
 
-const urls = ['https://verystream.com/e/FYTgmNnQ5HE/'];
+const urls = ['https://verystream.com/stream/3tngLkGr2pn/'];
 
 ScraperTester.fromStatic(VerystreamScraper)
-  .testUrlDetection(urls)
-  .testScraping(urls)
-  .run();
+    .testUrlDetection(urls)
+    .testScraping(urls)
+    .run();
